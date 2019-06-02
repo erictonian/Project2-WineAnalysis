de8d85b466073f26864f728419ec3be36bc22fe8
Eric Staveley <eric.staveley@gmail.com>
2019-06-02 15:29:23 -0500

    updated bar.js to have color enh for bars and matching bg to body css

--- static/js/bar.js
+++ static/js/bar.js
@@ -1,24 +1,56 @@
 async function buildBar(country) {
 
     //using d3.json to fetch the country data for the plot
-    // console.log("in buildCharts:" + country)
-    //console.log(trace)
-    //d3.json(`/varieties/${country}`).then(function (trace) {
-    //console.log(trace)    //peek at the data
-
     let varietyURL = await d3.json(`/varieties/${country}`);
-    // console.log("in buildCharts--bar chart")
-    // console.log("country:" + country)
-    // console.log(data)
-    //const layout = { margin: { t: 30, b: 100 } };
-    //Plotly.plot("bar", data, layout);
+
     const variety = varietyURL.variety
     const counts = varietyURL.count
+    const itemsReturned = variety.length
+
+    //console.log(variety)
+    //console.log(itemsReturned)
+    //console.log(variety[0])
+
+    //build a color array to put in color for varieties that we want
+    //to be a certain color in our chart
+    //Top10 value counts in world:  
+    //Pinot Noir, Cabernet Sauvignon, Chardonnay, Syrah, Red Blend,
+    //Zinfandel, MErlot, Sauvignon Blanc, Bordeaux-style Red Blend, Riesling
+    //anthing else will be a medium blue/green
+    //
+    var count = 0;
+    colorArray = []
+    for (var i = 0; i < itemsReturned; ++i) {
+        if (variety[i] == 'Chardonnay') {
+            colorChard = 'rgba(255,200,120,0.8)'
+            colorArray.push(colorChard)
+        } else if (variety[i] == 'Red Blend') {
+            colorRedBlend = 'rgba(114,47,55,0.8)'
+            colorArray.push(colorRedBlend)
+        } else if (variety[i] == 'Cabernet Sauvignon') {
+            colorCab = 'rgba(88,11,28,0.8)'
+            colorArray.push(colorCab)
+        } else if (variety[i] == 'Pinot Noir') {
+            colorPinotNoir = 'rgba(120,13,48,0.8)'
+            colorArray.push(colorPinotNoir)
+        } else {
+            //put any non matching colors here
+            //colorBasecolor = 'rgba(204,204,204,1)'
+            colorBasecolor = 'rgba(0,103,120,1)'      //jacksonville jag teal
+            colorArray.push(colorBasecolor)
+        }
+
+    }
+    //console.log(colorArray)
 
     const varietyTrace = {
         "x": variety,
         "y": counts,
-        "type": "bar"
+        "type": "bar",
+        marker: {
+            color: colorArray
+        },
+
     }
 
     const varietyData = [varietyTrace]
@@ -28,11 +60,13 @@ async function buildBar(country) {
             t: 100,
             b: 100
         },
-        showlegend: true,
-        title: "Top 10 Variety Counts"
+        showlegend: false,
+        title: "Top 10 Variety Counts",
+        paper_bgcolor: 'rgba(253, 252, 249,1)',  //  match to style.css  #fdfcf9
+        plot_bgcolor: 'rgba(253, 252, 249,1)',
     };
+
 
     Plotly.newPlot("bar", varietyData, layout);
 }
 
-//end of d3.json fetch country data
